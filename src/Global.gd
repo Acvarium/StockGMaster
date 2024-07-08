440d72978abf7b04a143211bf6b621a45d0f748b
Vitalii Shmorhun <3dvits@gmail.com>
2024-07-09 00:14:17 +0300

    Implemented a basis for saving and loading of the application settings

--- src/Global.gd
+++ src/Global.gd
@@ -1,4 +1,7 @@
 extends Node
+var save_file = "user://StockGMaster.conf.json"
+var profile_names = ['default']
+var current_profile_id = 0
 
 enum WhatToDo {
 	None,
@@ -18,3 +21,28 @@ enum ActionDataType {
 	Stock
 }
 
+
+func load_game():
+	var file = FileAccess.open(save_file, FileAccess.READ)
+	if is_instance_valid(file):
+		var data = JSON.parse_string(file.get_as_text())
+		if "profile_names" in data:
+			profile_names = data.profile_names
+		if "current_profile_id" in data:
+			current_profile_id = data.current_profile_id
+	else:
+		default_values()
+
+
+func save_config():
+	var data = {}
+	data.profile_names = profile_names
+	data.current_profile_id = current_profile_id
+	var file = FileAccess.open(save_file, FileAccess.WRITE)
+	file.store_string(JSON.stringify(data, "\t"))
+	file = null
+
+
+func default_values():
+	profile_names = ['default']
+	current_profile_id = 1
